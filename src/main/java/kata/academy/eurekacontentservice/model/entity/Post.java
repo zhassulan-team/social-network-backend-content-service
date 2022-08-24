2d8d3b8cbed23cdfa96f5413da0ea3e24d1a3c0d
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-08-24 15:26:30 +0600

    fix Post

--- src/main/java/kata/academy/eurekacontentservice/model/entity/Post.java
+++ src/main/java/kata/academy/eurekacontentservice/model/entity/Post.java
@@ -5,16 +5,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -38,8 +45,10 @@ public class Post {
     private String text;
 
     @ElementCollection
-    @CollectionTable(name = "posts_tags",
-            joinColumns=@JoinColumn(name = "id_tags"))
+    @CollectionTable(
+            name = "posts_tags",
+            joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"))
+    @Column(name = "tag", nullable = false)
     private List<String> tags = new ArrayList<>();
 
     @Override
@@ -55,4 +64,3 @@ public class Post {
         return Objects.hash(id, userId, title, text, tags);
     }
 }
-
---
+++ src/main/java/kata/academy/eurekacontentservice/service/impl/PostServiceImpl.java
@@ -0,0 +1,37 @@
+package kata.academy.eurekacontentservice.service.impl;
+
+import kata.academy.eurekacontentservice.model.entity.Post;
+import kata.academy.eurekacontentservice.repository.PostRepository;
+import kata.academy.eurekacontentservice.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Override
+    public Post addPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post updatePost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public void deleteById(Long postId) {
+        postRepository.deleteById(postId);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public boolean existsByIdAndUserId(Long postId, Long userId) {
+        return postRepository.existsByIdAndUserId(postId, userId);
+    }
+}
