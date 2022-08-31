5eabc26f51a828af7c5d3afd75cd80b802ba2256
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-08-31 18:50:11 +0600

    fix dto

--- src/main/java/kata/academy/eurekacontentservice/model/dto/CommentResponseDto.java
+++ src/main/java/kata/academy/eurekacontentservice/model/dto/CommentResponseDto.java
@@ -1,12 +1,8 @@
 package kata.academy.eurekacontentservice.model.dto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 public record CommentResponseDto(
         Long id,
         Long userId,
         String text,
-        Long postId
-) {
+        Long postId) {
 }
