fafd7b4ee7f5f3c45f520de6bf270b36cc8a9d4c
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-09-12 17:37:51 +0600

    fix

--- src/main/java/kata/academy/eurekacontentservice/feign/LikeServiceFeignClient.java
+++ src/main/java/kata/academy/eurekacontentservice/feign/LikeServiceFeignClient.java
@@ -1,6 +1,5 @@
 package kata.academy.eurekacontentservice.feign;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
