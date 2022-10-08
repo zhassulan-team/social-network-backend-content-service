b8bf81ffaa7bfd0d70876e490d61f73cd06b110c
Vitaly Kravchenko <dearvito@yandex.com>
2022-10-08 12:08:34 +0300

    BACK33 add swagger API documentation on outer REST controllers.
    BACK42 add swagger API documentation on outer REST controllers.

--- src/main/java/kata/academy/eurekacontentservice/rest/outer/CommentRestController.java
+++ src/main/java/kata/academy/eurekacontentservice/rest/outer/CommentRestController.java
@@ -1,5 +1,11 @@
 package kata.academy.eurekacontentservice.rest.outer;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kata.academy.eurekacontentservice.api.Response;
 import kata.academy.eurekacontentservice.model.converter.CommentMapper;
 import kata.academy.eurekacontentservice.model.dto.CommentPersistRequestDto;
@@ -33,12 +39,29 @@ import java.util.Optional;
 @Validated
 @RestController
 @RequestMapping("/api/v1/posts")
+@Tag(name = "Comment", description = "The Comment API")
 public class CommentRestController {
 
     private final CommentService commentService;
     private final PostService postService;
     private final CommentResponseDtoService commentResponseDtoService;
 
+    @Operation(summary = "Create a new comment",
+            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = CommentPersistRequestDto.class))}),
+            parameters = {
+                    @Parameter(name = "postId", description = "Post id", required = true),
+                    @Parameter(name = "userId", description = "User id", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "New comment has been created successfully",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CommentPersistRequestDto.class))
+                            }),
+                    @ApiResponse(responseCode = "400", description = "Post with postId does not exists",
+                            content = @Content)
+            }
+    )
     @PostMapping("/{postId}/comments")
     public Response<CommentResponseDto> addComment(@RequestBody @Valid CommentPersistRequestDto dto,
                                                    @PathVariable @Positive Long postId,
@@ -51,6 +74,23 @@ public class CommentRestController {
         return Response.ok(CommentMapper.toDto(commentService.addComment(comment)));
     }
 
+    @Operation(summary = "Update comment",
+            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = CommentUpdateRequestDto.class))}),
+            parameters = {
+                    @Parameter(name = "postId", description = "Post id", required = true),
+                    @Parameter(name = "commentId", description = "Comment id", required = true),
+                    @Parameter(name = "userId", description = "User id", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Comment has been updated successfully",
+                            content = {@Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CommentUpdateRequestDto.class))
+                            }),
+                    @ApiResponse(responseCode = "400", description = "Comment with commentId, postId and userId does not exists",
+                            content = @Content)
+            }
+    )
     @PutMapping("/{postId}/comments/{commentId}")
     public Response<CommentResponseDto> updateComment(@RequestBody @Valid CommentUpdateRequestDto dto,
                                                       @PathVariable @Positive Long postId,
@@ -61,6 +101,19 @@ public class CommentRestController {
         return Response.ok(CommentMapper.toDto(commentService.updateComment(CommentMapper.toEntity(dto, commentOptional.get()))));
     }
 
+    @Operation(summary = "Delete comment",
+            parameters = {
+                    @Parameter(name = "postId", description = "Post id", required = true),
+                    @Parameter(name = "commentId", description = "Comment id", required = true),
+                    @Parameter(name = "userId", description = "User id", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Comment has been deleted successfully",
+                            content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Comment with commentId, postId and userId does not exists",
+                            content = @Content)
+            }
+    )
     @DeleteMapping("/{postId}/comments/{commentId}")
     public Response<Void> deleteComment(@PathVariable @Positive Long postId,
                                         @PathVariable @Positive Long commentId,
@@ -70,6 +123,19 @@ public class CommentRestController {
         return Response.ok();
     }
 
+    @Operation(summary = "Get page with comments",
+            parameters = {
+                    @Parameter(name = "postId", description = "Post id", required = true),
+                    @Parameter(name = "userId", description = "User id", required = true),
+                    @Parameter(name = "pageable", description = "Pagination information", required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Comment page created successfully",
+                            content = @Content),
+                    @ApiResponse(responseCode = "400", description = "Post with postId does not exists",
+                            content = @Content)
+            }
+    )
     @GetMapping("/{postId}/comments")
     public Response<Page<CommentResponseDto>> getCommentPage(@PathVariable @Positive Long postId,
                                                              @RequestParam @Positive Long userId,
