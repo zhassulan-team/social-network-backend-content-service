9dd1912a812980502a6bf25661e5f04380e8f4ae
AlexanderVunder <alvund1994@gmail.com>
2022-10-28 12:43:55 +0700

    fix test CommentRestControllerIT, method getCommentPage_SuccessfulTest

--- src/test/java/kata/academy/eurekacontentservice/outer/CommentRestControllerIT.java
+++ src/test/java/kata/academy/eurekacontentservice/outer/CommentRestControllerIT.java
@@ -20,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @MockBeans({
@@ -47,7 +48,6 @@ public class CommentRestControllerIT extends SpringSimpleContextTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.text", Is.is(commentResponseDto.text())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.postId", Is.is(commentResponseDto.postId().intValue())));
 
-        assertTrue(commentResponseDto.createdDate().isBefore(LocalDateTime.now()));
         assertTrue(entityManager.createQuery(
                 """
                         SELECT COUNT(cl.id) > 0
@@ -92,7 +92,6 @@ public class CommentRestControllerIT extends SpringSimpleContextTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.text", Is.is(commentResponseDto.text())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.postId", Is.is(commentResponseDto.postId().intValue())));
 
-        assertTrue(commentResponseDto.createdDate().isBefore(LocalDateTime.now()));
         assertTrue(entityManager.createQuery(
                         """
                                 SELECT COUNT(cl.id) > 0
@@ -166,17 +165,76 @@ public class CommentRestControllerIT extends SpringSimpleContextTest {
     public void getCommentPage_SuccessfulTest() throws Exception {
 
         Long postId = 1L;
+
+        CommentResponseDto[] sortedByText = {
+                new CommentResponseDto(3L, 3L, "All Right!That’s the word on the street, isn’t it?", postId, LocalDateTime.now()),
+                new CommentResponseDto(1L, 3L, "Black and white show!", postId, LocalDateTime.now()),
+                new CommentResponseDto(2L, 1L, "Could you tell me why you applied for this job?", postId, LocalDateTime.now()),
+                new CommentResponseDto(4L, 2L, "Did you feel it?", postId, LocalDateTime.now())
+
+        };
+
+        CommentResponseDto[] sortedById = {
+                new CommentResponseDto(3L, 3L, "All Right!That’s the word on the street, isn’t it?", postId, LocalDateTime.now()),
+                new CommentResponseDto(6L,4L,"Right!",postId,LocalDateTime.now()),
+                new CommentResponseDto(5L,5L,"Terrific!That’s the word on the street, isn’t it?",postId,LocalDateTime.now())
+
+        };
+
         mockMvc.perform(get("/api/v1/content/posts/{postId}/comments", postId)
-                .contentType(MediaType.APPLICATION_JSON))
+                        .param("page", "0")
+                        .param("size", "4")
+                        .param("sort", "text")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].id", Is.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].id", Is.is(sortedByText[0].id().intValue())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].postId", Is.is(postId.intValue())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].text", Is.is("qwerty")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].userId", Is.is(1)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].id", Is.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].text", Is.is(sortedByText[0].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].userId", Is.is(sortedByText[0].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].id", Is.is(sortedByText[1].id().intValue())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].postId", Is.is(postId.intValue())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].text", Is.is("Hello")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].userId", Is.is(2)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].text", Is.is(sortedByText[1].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].userId", Is.is(sortedByText[1].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].id", Is.is(sortedByText[2].id().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].postId", Is.is(postId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].text", Is.is(sortedByText[2].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].userId", Is.is(sortedByText[2].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[3].id", Is.is(sortedByText[3].id().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[3].postId", Is.is(postId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[3].text", Is.is(sortedByText[3].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[3].userId", Is.is(sortedByText[3].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.sort.sorted", Is.is(true)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.pageNumber", Is.is(0)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.pageSize", Is.is(4)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages", Is.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements", Is.is(6)));
+
+        mockMvc.perform(get("/api/v1/content/posts/{postId}/comments", postId)
+                        .param("page", "1")
+                        .param("size", "3")
+                        .param("sort", "userId")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].id", Is.is(sortedById[0].id().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].postId", Is.is(postId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].text", Is.is(sortedById[0].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[0].userId", Is.is(sortedById[0].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].id", Is.is(sortedById[1].id().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].postId", Is.is(postId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].text", Is.is(sortedById[1].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[1].userId", Is.is(sortedById[1].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].id", Is.is(sortedById[2].id().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].postId", Is.is(postId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].text", Is.is(sortedById[2].text())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.[2].userId", Is.is(sortedById[2].userId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.sort.sorted", Is.is(true)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.pageNumber", Is.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.pageable.pageSize", Is.is(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages", Is.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements", Is.is(6)));
+
     }
 
     @Test
