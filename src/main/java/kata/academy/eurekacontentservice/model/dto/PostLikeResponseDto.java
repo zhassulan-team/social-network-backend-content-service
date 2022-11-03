036510c990e7774da10df62e73017a801c4e2873
chmel999 <chmel999@rambler.ru>
2022-11-03 15:28:08 +0300

    BACK45 Меняет возвращаемые значения рест контрллера на PostResponseDto

--- src/main/java/kata/academy/eurekacontentservice/model/dto/PostLikeResponseDto.java
+++ src/main/java/kata/academy/eurekacontentservice/model/dto/PostLikeResponseDto.java
@@ -1,14 +1,8 @@
 package kata.academy.eurekacontentservice.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-public class PostLikeResponseDto {
-    Long postId;
-    Integer positiveLikesCount;
-    Integer negativeLikesCount;
+public record PostLikeResponseDto(
+        Long postId,
+        Integer positiveLikesCount,
+        Integer negativeLikesCount) {
 }
